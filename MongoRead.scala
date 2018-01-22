94bf3dd8747e7b9a9dc29f8801530b500a4536ac
conterasda <35156796+conterasda@users.noreply.github.com>
2018-01-22 17:30:04 +0700

    Add files via upload

--- MongoRead.scala
+++ MongoRead.scala
@@ -6,10 +6,8 @@ import org.bson.Document
 import com.mongodb.spark._
 import com.mongodb.spark.config._
 import com.mongodb.spark.rdd.MongoRDD
-import com.mongodb.util.JSONParser
-
 import scala.collection.mutable.ListBuffer
-import scala.util.parsing.json.{JSONArray, JSONObject}
+import scala.util.parsing.json.{JSON, JSONArray, JSONObject}
 
 class MongoRead extends BaseContext with Serializable {
   def readDB(sc: SparkContext, database: String,
@@ -49,27 +47,31 @@ class MongoRead extends BaseContext with Serializable {
     return aggRdd
   }
 
-//  def parser(cmd: String) {
-//    var cmd2 = cmd
-//    println(cmd2)
-//    val cmdParsed = JSONParser.
-//
-//    cmdParsed match {
-//      // Matches if jsonStr is valid JSON and represents a Map of Strings to Any
-//      case Some(map: Any) => {
-//        var arr = map.asInstanceOf[JSONArray]
-//        val s = arr.list.map {
-//          x =>
-//            val y = x.asInstanceOf[JSONObject]
-//            println(y.toString() + "hi")
-//            Document.parse(y.toString())
-//        }
-//
-//      }
-//      case None => println("Parsing failed")
-//      case other => println("Unknown data structure: " + other)
-//    }
-//  }
+  def parser(sc: SparkContext,database:String, collection:String,cmd:String) {
+    val readConfig = ReadConfig(Map("uri" ->
+      ("mongodb://localhost:27017/" + database + "." + collection)))
+    //+ "?replicaSet=PMArepl")))
+    var rdd = sc.loadFromMongoDB(readConfig)
+
+    val cmdParsed = JSON.parseRaw(cmd)
+
+    cmdParsed match {
+      // Matches if jsonStr is valid JSON and represents a Map of Strings to Any
+      case Some(map: Any) => {
+        var arr = map.asInstanceOf[JSONArray]
+        val s = arr.list.map {
+          x =>
+            val y = x.asInstanceOf[JSONObject]
+            println(y.toString())
+            Document.parse(y.toString())
+        }
+        rdd.withPipeline(s).toDF().show()
+
+      }
+      case None => println("Parsing failed")
+      case other => println("Unknown data structure: " + other)
+    }
+  }
 }
 
 object MongoRead {
@@ -79,39 +81,32 @@ object MongoRead {
     Logger.getLogger("akka").setLevel(Level.OFF)
     val sc = new SparkContext(conf)
 
-    var s = "db.test_1.aggregate([{ $match: { $or: [ { SaleNo: { $gt: 20, $lt: 40 } },{Name: { $eq: \"cong1\" } } ] } },{ $group:{_id:{Department:\"$Department\",ID:\"$ID\"},      TotalSaleNo:{$sum:\"$SaleNo\"},Count:{$sum:1}}},    { $project:{\"_id.ID\":1,\"_id.Department\":1,Count:1,TotalSaleNo:1}}])"
+    var s = "db.test_1.aggregate([{ $match: { $or: [ { SaleNo: { $gt: 20, $lt: 40 } },{Name: { $eq: \"cong1\" } } ] } },        { $group:{_id:{Department:\"$Department\",ID:\"$ID\"},  TotalSaleNo:{$sum:\"$SaleNo\"},Count:{$sum:1}}},{ $project:{\"_id.ID\":1,\"_id.Department\":1,Count:1,TotalSaleNo:1}}])"
 
     s = s.replaceAll("\\s", "")
-
-//    check command start with db.
-
+    //    check command start with db.
     s = s.drop(3)
-
     val collection = s.substring(0, s.indexOf('.'))
-    // println(collection)
+
     s = s.drop(s.indexOf('.') + 1)
 
-    if (s.substring(0, 9) == "aggregate") {
-      s = s.drop(9)
+    if (s.substring(0, s.indexOf('(')) == "aggregate") {
+      s = s.drop(s.indexOf('(') + 2).dropRight(2)
     }
-    s = s.dropRight(2)
-    s = s.drop(2)
-    //    println(s)
+
+    println(s)
 
     val mongoRead = new MongoRead()
-//    mongoRead.parser(s)
-
-    //    for (a <- args) println(a)
-    //    val queries = args.toList.drop(2)
-
     val rows = mongoRead.readDB(sc, "admin", collection, s).toDF().show()
-    //    val queries = new ListBuffer[String]()
-    //    queries += "{ $match: { $or: [ { SaleNo: { $gt: 20, $lt: 40 } },{Name: { $eq: \"cong1\" } } ] } }"
-    //    queries += "{$group:{_id:{Department:\"$Department\",ID:\"$ID\"},TotalSaleNo:{$sum:\"$SaleNo\"},Count:{$sum:1}}}"
-    //    queries+= "{$project:{\"_id.ID\":1,\"_id.Department\":1,Count:1,TotalSaleNo:1}}"
-    //
-    //    val rows = mongoRead.readDB(sc, "admin", "test_1", queries)
-    //    rows.toDF().show(100)
 
+//    val s1 =
+//      """[{"$match": { "SaleNo": { "$gt": 20, "$lt": 40 } }},
+//        |{ "$group":{"_id":{"Department":"$Department","ID":"$ID"},"TotalSaleNo":{"$sum":"$SaleNo"},"Count":{"$sum":1}}},
+//        |{ "$project":{"_id.ID":1,"_id.Department":1,"Count":1,"TotalSaleNo":1}}]""".stripMargin
+//
+//    mongoRead.parser(sc,"admin",collection,s1)
+
+
+    sc.stop()
   }
 }
